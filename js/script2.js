2fbda9d88663470b2ddd4426a0bd56a371438434
rontomato <67863002+rontomato@users.noreply.github.com>
2020-07-06 19:52:41 +0800

    coffee dashboard project
    a simple coffee dashboard with js

---
+++ js/script2.js
@@ -0,0 +1,23 @@
+
+const showRequiredCategory = event => {
+    const getId = event.id
+    const links = document.querySelectorAll('.design-category button')
+    for(i = 0; i < links.length; i++){
+        if(links[i].hasAttribute('class')) {
+            links[i].classList.remove('active')
+        }
+    }
+
+    event.classList.add('active')
+    const getCategory = document.querySelector(`.category-${getId}`)
+    const categories = document.querySelectorAll('div[class ^= "category-"]')
+    for(i=0; i<categories.length; i++){
+        if(categories[i].hasAttribute('class')){
+            categories[i].classList.remove('showCategory')
+            categories[i].classList.add('hideCategory')
+            
+        }
+    }
+    getCategory.classList.remove('hideCategory')
+    getCategory.classList.add('showCategory')
+}
